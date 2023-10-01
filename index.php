49490b3839b5dbfcc5185f1aff983ab98da3372f
harvey <40221196+bido101@users.noreply.github.com>
2023-10-01 20:26:45 +0800

    upload all changes

---
+++ index.php
@@ -0,0 +1,238 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>EasTDev PH</title>
+	<link href="assets/css/mystyle.css" rel="stylesheet">
+	<link href="assets/css/bootstrap.min.css" rel="stylesheet">
+	<style type="text/css">
+		body{
+			background: url(assets/img/bg_Eastdevph.png) no-repeat center center fixed; 
+			-webkit-background-size: cover;
+			-moz-background-size: cover;
+			-o-background-size: cover;
+			background-size: cover;
+
+		}
+		.nav-link{
+			font-weight: bold;
+			font-size: 18px;
+		}
+	</style>
+</head>
+<body>
+	<!-- navbar -->
+		<nav class="navbar sticky-top navbar-expand-lg" style="background-color: #D9EEE1!important;color: solid black;">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+		      <img src="assets/img/eastdev_logo.png" alt="Logo" width="40" height="45" class="d-inline-block align-text-top">
+		      EasTDev Ph
+	    	</a>
+        <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="navbar-collapse collapse justify-content-end" id="navbarScroll" style="">
+          <ul class="navbar-nav nav-underline" style="--bs-scroll-height: 100px;">
+            <li class="nav-item">
+              <a class="nav-link" aria-current="page" href="#home">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#service">Service</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#team">Team</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#aboutus">About Us</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#contactus">Contact Us</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  <!-- navbar -->
+  <!-- home section -->
+		<section id="home">
+			<img src="assets/img/Eastdev_ph__1_-removebg.png" style="width: 100%;height: auto;">
+		</section>
+		<br>
+  <!-- home section -->
+  <!-- service section -->
+		<section id="service">
+			<br><br><br>
+			<div class="card">
+			  <div class="card-body">
+			    <div align="center">
+			    	<label style="font-size: 20px;"><b>Web host & Design</b></label>
+					  <div class="card" style="border: none;width: 85%;">
+						  <div class="card-body">
+						    <p style="color: #1F628E;">
+							    	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+							    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+							    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+							    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+							    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+							    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+						  	</p>
+						  </div>
+						</div>  
+			    </div>
+				  <div align="center">
+			    	<label style="font-size: 20px;"><b>System Development</b></label>
+					  <div class="card" style="border: none;width: 85%;">
+						  <div class="card-body">
+						    <p style="color: #1F628E;">
+							    	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+							    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+							    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+							    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+							    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+							    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+						  	</p>
+						  </div>
+						</div>  
+			    </div>
+			  </div>
+			</div>
+		</section>
+		<br>
+	<!-- service section -->
+	<!-- team section -->
+		<section id="team">
+			<br><br><br>
+			<div class="card">
+				<div class="card-body">
+					<div id="myCarousel" class="carousel slide container" data-bs-ride="carousel">
+				    <div class="carousel-inner w-100">
+				        <div class="carousel-item active">
+				            <div class="col-md-4">
+				                <div class="card card-body" style="background-color: #D9EEE1!important;" align="center">
+				                    <img class="img-fluid" src="assets/img/team/62256825_2422099241189313_3329312434287542272_n.jpg">
+				                    <label><b>Project Manager</b></label>
+				                </div>
+				            </div>
+				        </div>
+				        <div class="carousel-item">
+				            <div class="col-md-4">
+				                <div class="card card-body" style="background-color: #D9EEE1!important;" align="center">
+				                    <img class="img-fluid" src="assets/img/team/356889622_3621945021366973_1506808033036755306_n.png">
+				                    <label><b>Full Stack Developer</b></label>
+				                </div>
+				            </div>
+				        </div>
+				        <div class="carousel-item">
+				            <div class="col-md-4">
+				                <div class="card card-body" style="background-color: #D9EEE1!important;" align="center">
+				                    <img class="img-fluid" src="assets/img/team/345883926_2178364415688268_6931514675644451300_n.jpg">
+				                    <label><b>Full Stack Developer</b></label>
+				                </div>
+				            </div>
+				        </div>
+				        <div class="carousel-item">
+				            <div class="col-md-4">
+				                <div class="card card-body" style="background-color: #D9EEE1!important;" align="center">
+				                    <img class="img-fluid" src="assets/img/team/69400594_2549008955222347_6088068480827916288_n.jpg">
+				                    <label><b>Documentaion/Web Designer</b></label>
+				                </div>
+				            </div>
+				        </div>
+				        <div class="carousel-item">
+				            <div class="col-md-4">
+				                <div class="card card-body" style="background-color: #D9EEE1!important;" align="center">
+				                    <img class="img-fluid" src="assets/img/team/310099037_5545025158914475_4232332766406525663_n.jpg">
+				                    <label><b>Documentaion/Web Designer</b></label>
+				                </div>
+				            </div>
+				        </div>
+				    </div>
+				    <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+				        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				        <span class="visually-hidden">Previous</span>
+				    </button>
+				    <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+				        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+				        <span class="visually-hidden">Next</span>
+				    </button>
+				  </div>
+				</div>
+			</div>
+		</section>
+		<br>
+	<!-- team section -->
+	<!-- about us section -->
+		<section id="aboutus">
+			<br><br><br>
+			<div class="card">
+				<div class="card-body" align="center">
+					<h3><u>About Us</u></h3>
+					<br>
+					<img src="assets/img/Eastdev_ph_about us.png">
+					<div class="card" style="border: none;width: 50%;">
+						  <div class="card-body">
+								<p style="color: #1F628E;">
+										EasTDev is a team that provides ICT solution, support and services. We are committed in providing excellent client services. We have the ability to draw from an experienced base of personnel whilst maintaining a personalized form of business.
+								</p>
+							</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<br>
+	<!-- about us section -->
+	<!-- contact us section -->
+		<section id="contactus">
+			<br><br><br>
+			<div class="card">
+				<div class="card-body">
+					<h3 align="center"><u>Contact Us</u></h3>
+					<br>
+
+					<div class="row"> 
+						<div class="col-md-6">
+							<div class="card" style="border-color: #145DA0;">
+								<div class="card-body">
+									<form>
+										<input class="form-control" type="text" placeholder="Name...">
+										<br>
+										<input class="form-control" type="email" placeholder="Email...">
+										<br>
+										<textarea class="form-control" placeholder="Message..."></textarea>
+										<br>
+										<button class="btn btn-success form-control">Submit</button>
+									</form>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-6" align="center">
+							<img src="assets/img/contact_logo.png" width="140" height="100">
+							<br>
+							<br>
+							<label><b>Email us:</b> eastdev555@gmail.com</label>
+							<br>
+							<br>
+							<label><b>Text/Call Us:</b> +63938 477 4207</label>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	<!-- contact us section -->
+	<!-- script -->
+	<script src="assets/js/bootstrap.bundle.min.js"></script>
+	<script src="assets/js/jquery.min.js"></script>
+	<script type="text/javascript">
+		$('.carousel .carousel-item').each(function () {
+			var minPerSlide = 3;
+			var next = $(this).next();
+			if (!next.length) {
+			next = $(this).siblings(':first');
+			}
+			next.children(':first-child').clone().appendTo($(this));
+
+			for (var i = 0; i < minPerSlide; i++) { next=next.next(); if (!next.length) { next=$(this).siblings(':first'); } next.children(':first-child').clone().appendTo($(this)); } });
+	</script>
+</body>
+</html>
